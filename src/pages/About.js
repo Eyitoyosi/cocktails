fdeec8d01cc7c4ac80b7b9afdedefe8c35fab11f
eyitoyosi <harlahbee@gmail.com>
2022-10-12 20:57:43 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Eius modi animi odit aperiam facere aliquid tempora mollitia quae perferendis odio adipisci 
+        delectus expedita molestias doloremque voluptate, excepturi reiciendis, 
+        officiis dolore qui repudiandae quas? 
+        Asperiores, blanditiis deleniti ipsum impedit maiores alias?
+      </p>
+    </section>
+  )
+}
+
+export default About
